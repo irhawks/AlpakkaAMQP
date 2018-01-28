3071684c6e9a960bc7994209b2d89d401c3e39c5
hawk <irhawks@163.com>
2018-01-28 19:24:07 +0800

    update to sbt 1.0

--- src/main/scala/com/abhi/AlpakkaAMQPPublisher.scala
+++ src/main/scala/com/abhi/AlpakkaAMQPPublisher.scala
@@ -15,35 +15,47 @@ import scala.concurrent.duration._
 import akka.stream.scaladsl._
 
 /**
-  * Created by ASrivastava on 10/2/17.
-  */
+ * Created by ASrivastava on 10/2/17.
+ */
 object AlpakkaAMQPPublisher extends App {
-   implicit val actorSystem = ActorSystem()
-   implicit val actorMaterializer = ActorMaterializer()
-   val queueName = "myqueue"
-   val queueDeclaration = QueueDeclaration(queueName, durable = true)
-   val uri = "amqp://abhi:abhi@abhisheks-mini:5672/myvhost"
-   val settings = AmqpSinkSettings(AmqpConnectionUri(uri))
-      .withRoutingKey("foobar")
-      .withExchange("exchange")
-      .withDeclarations(queueDeclaration)
-   val amqpSink = AmqpSink.simple(settings)
 
-   val resource = getClass.getResource("/countrycapital.csv")
-   val path = Paths.get(resource.toURI)
-   val source = FileIO.fromPath(path)
+  implicit val actorSystem = ActorSystem()
+  implicit val actorMaterializer = ActorMaterializer()
 
-   val graph = RunnableGraph.fromGraph(GraphDSL.create(amqpSink){implicit builder =>
-      s =>
-         import GraphDSL.Implicits._
-         source ~> s.in
-         ClosedShape
-   })
+  val queueDeclaration = QueueDeclaration("queue", durable = true)
 
-   val future = graph.run()
+  //val exchangeName = "amqp-conn-it-spec-pub-sub-" + System.currentTimeMillis()
+  val exchangeName = "amqp-nb"
+  val exchangeDeclaration = ExchangeDeclaration(exchangeName, "fanout")
 
-   future.onComplete { _ =>
-      actorSystem.terminate()
-   }
-   Await.result(actorSystem.whenTerminated, Duration.Inf)
+  val uri = "amqp://172.17.0.1:5672/"
+
+  val connectionProvider =
+    AmqpDetailsConnectionProvider(List(("172.17.0.1", 5672)))
+      .withHostsAndPorts(("172.17.0.1", 5672))
+
+      val amqpSink = AmqpSink.simple(
+        AmqpSinkSettings(connectionProvider)
+          .withRoutingKey("")
+          .withExchange(exchangeName)
+          .withDeclarations(exchangeDeclaration)
+        )
+
+      val resource = getClass.getResource("/countrycapital.csv")
+      val path = Paths.get(resource.toURI)
+      val source = FileIO.fromPath(path)
+
+      val graph = RunnableGraph.fromGraph(GraphDSL.create(amqpSink){implicit builder =>
+        s =>
+          import GraphDSL.Implicits._
+          source ~> s.in
+          ClosedShape
+      })
+
+      val future = graph.run()
+
+      future.onComplete { _ =>
+        actorSystem.terminate()
+      }
+      Await.result(actorSystem.whenTerminated, Duration.Inf)
 }
